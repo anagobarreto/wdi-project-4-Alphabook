fc1227497ca0997ffe4128bc55b452edae2e2685
Anacgbarreto <anacgbarreto@gmail.com>
2017-06-04 23:02:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const statuses = [
+  {
+    id: 1,
+    name: "Ana Barreto",
+    profilePic: "/profilepic.jpg",
+    text: "This is a status",
+    likeCount: 5,
+    liked: false,
+  },
+  {
+    id: 2,
+    name: "Ana McKenzie",
+    profilePic: "/profilepic.jpg",
+    text: "This is a status tasdfasdf",
+    likeCount: 8,
+    liked: true,
+  }
+];
+
+class Status extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {liked: props.liked};
+  }
+
+  render() {
+    const {props} = this;
+    return (
+      <article className="status">
+        <div className="status-container">
+          <p className="status-line">
+            <img className="profile-pic" src={props.profilePic} />
+            <span>{props.name}</span>
+          </p>
+          <p className="status-text">
+            {props.text}
+          </p>
+          <p className="status-likes">
+            {props.likeCount} likes
+          </p>
+        </div>
+        <ul className="status-buttons">
+          <li onClick={() => {
+            this.setState({
+              liked: !this.state.liked,
+            });
+          }}>
+            {this.state.liked ? 'Unlike' : 'Like'}
+          </li>
+          <li>Comment</li>
+        </ul>
+      </article>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Anabook</h1>
+          <ul>
+            <li>Profile</li>
+            <li>Logout</li>
+          </ul>
+        </header>
+        <main>
+          <aside>
+            I am a sidebar
+          </aside>
+          <section>
+            {statuses.map(status => {
+              return <Status key={status.id} {...status} />;
+            })}
+          </section>
+        </main>
+        <footer>Website by Ana Barreto</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
