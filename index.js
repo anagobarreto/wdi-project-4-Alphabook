045f5c86b67434c9c215a0b2b48e362ca91b73c3
Anacgbarreto <anacgbarreto@gmail.com>
2017-06-05 14:36:49 +0100

    Adding passport

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./config');
+
+require('./server/models').connect(config.dbUri);
 
 const app = express();
-
 app.use(express.static('./server/static/'));
-app.use(express.static('/client/dist/'));
+app.use(express.static('./client/dist/'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
 
-app.use(boydParser.urlencoded({ extended: false }));
+const localSignupStrategy = require('./server/passport/local-signup');
+const localLoginStrategy = require('./server/passport/local-login');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+const authCheckMiddleware = require('./server/middleware/auth-check');
+app.use('/api', authCheckMiddleware);
 
 const authRoutes = require('./server/routes/auth');
+const apiRoutes = require('./server/routes/api');
 app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(3000, () => {
-  console.log('server is running on http://localhost:3000')
+  console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
 });
---
+++ server/models/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+module.exports.connect = (uri) => {
+  mongoose.connect(uri);
+  mongoose.Promise = global.Promise;
+
+
+  mongoose.connection.on('error', (err) => {
+    console.error(`Mongoose connection error: ${err}`);
+    process.exit(1);
+  });
+
+  require('./user');
+};
