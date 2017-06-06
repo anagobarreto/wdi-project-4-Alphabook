588bb12d0214a30d11b0db4df37c541b2b5080dd
Anacgbarreto <anacgbarreto@gmail.com>
2017-06-06 15:53:12 +0100

    More auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -97,7 +97,30 @@ router.post('/signup', (req, res) => {
     });
   }
 
-  return res.status(200).end();
+  return passport.authentication('local-signup', (err) => {
+    if (err) {
+      if (err.name === 'MongoError' && err.code === 11000) {
+        return res.status(409).json({
+          success: false,
+          message: 'Check the form for errors.',
+          errors: {
+            email: 'This email is already taken.'
+          }
+        });
+      }
+
+      return res.status(400).json({
+        success: false,
+        message: 'Could not process the form'
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: 'You have successfully signed up! Now you should be able to login'
+    });
+  })
+  (req, res, next);
 });
 
 router.post('/login', (req, res) => {
@@ -110,7 +133,29 @@ router.post('/login', (req, res) => {
     });
   }
 
-  return res.status(200).end();
+  return passport.authenticate('local-login', (err, token, userData) => {
+    if (err) {
+      if (err.name === 'IncorrectCredentialsError') {
+        return res.status(400).json({
+          success: false,
+          message: err.message
+        });
+      }
+      return res.status(400).json({
+        success: false,
+        message: 'Could not process the form'
+      });
+    }
+
+    return res.json({
+      success: true,
+      message: ' You have successfully logged in!',
+      token,
+      user: userData
+    });
+  })
+
+  (req, res, next)
 });
 
 module.exports = router;
