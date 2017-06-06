714b86cba3012ec49e6611eec631df334c9c7d98
Anacgbarreto <anacgbarreto@gmail.com>
2017-06-06 11:40:53 +0100

    More auth

---
+++ server/passport/local-login.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken');
+const User = require('mongoose').model('User');
+const PassportLocalStrategy = require('passport-local').Strategy;
+const config = require('../../config');
+
+module.exports = new PassportLocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  session: false,
+  passReqToCallback: true
+}, (req, email, password, done) => {
+  const userData = {
+    email: email.trim(),
+    password: password.trim()
+  };
+
+  return User.findOne({ email: userData.email }, (err, user) => {
+    if (err) { return done(err); }
+
+    if (!user) {
+      const error = new Error('Incorrect email or password');
+      error.name = 'IncorrectCredentialsError';
+
+      return done(error);
+    }
+
+    return user.comparePassword(userData.password, (passwordErr, isMatch) => {
+      if (err) { return done(err); }
+
+      if (!isMatch) {
+        const error = new Error('Incorrect email or password');
+        error.name = 'IncorrectCredentialsError';
+
+        return done(error);
+      }
+
+      const payload = {
+        sub: user._id
+      };
+
+      const token = jwt.sign(payload, config.jwtSecret);
+      const data = {
+        name: user.name
+      };
+
+      return done(null, token, data);
+    });
+  });
+});
