8391030b6c6542ae873770367b61258eec63f7b9
Anacgbarreto <anacgbarreto@gmail.com>
2017-06-06 14:39:34 +0100

    More auth

---
+++ server/middleware/auth-check.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const User = require('mongoose').model('User');
+const config = require('../../config');
+
+module.exports = (req, res, next) => {
+  if (!req.header.authorization) {
+    return res.status(401).end();
+  }
+
+  const token = req.headers.authorization.split(' ')[1];
+
+  return jwt.verify(token, config.jwtSecret, (err, decoded) => {
+    if (err) {
+      return res.status(401).end();
+    }
+    
+    const userId = decoded.sub;
+
+    return User.findById(userId, (userErr, user) => {
+      if (userErr || !user) {
+        return res.status(401).end();
+      }
+
+      return next();
+    });
+  });
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const validator = require('validator');
+const passport = require('passport');
 
 const router = new express.Router();
 
