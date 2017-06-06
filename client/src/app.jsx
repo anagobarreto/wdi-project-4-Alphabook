ac660a26d36c8ca3d5fb0291214ed1a8d013a395
Anacgbarreto <anacgbarreto@gmail.com>
2017-06-06 16:06:51 +0100

    More auth

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,13 +1,14 @@
-import React from ' react';
+import React from 'react';
 import ReactDom from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import getMuiTheme from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { browserHistory, Router } from 'react-router';
 import routes from './routes.js';
 
 injectTapEventPlugin();
 
 ReactDom.render((
-  <MuiThemeProvider muiTheme =
-))
-// Not continuing this as it'll be changed in the future.
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Router history={browserHistory} routes={routes} />
+  </MuiThemeProvider>), document.getElementById('react-app'));
---
+++ client/src/routes.js
@@ -0,0 +1,46 @@
+import Base from './components/Base.jsx';
+import HomePage from './components/HomePage.jsx';
+import DashboardPage from './containers/DashboardPage.jsx';
+import LoginPage from './containers/LoginPage.jsx';
+import SignUpPage from './containers/SignUpPage.jsx';
+import Auth from './modules/Auth';
+
+
+const routes = {
+  component: Base,
+  childRoutes: [
+
+    {
+      path: '/',
+      getComponent: (location, callback) => {
+        if (Auth.isUserAuthenticated()) {
+          callback(null, DashboardPage);
+        } else {
+          callback(null, HomePage);
+        }
+      }
+    },
+
+    {
+      path: '/login',
+      component: LoginPage
+    },
+
+    {
+      path: '/signup',
+      component: SignUpPage
+    },
+
+    {
+      path: '/logout',
+      onEnter: (nextState, replace) => {
+        Auth.deauthenticateUser();
+
+        replace('/');
+      }
+    }
+
+  ]
+};
+
+export default routes;
